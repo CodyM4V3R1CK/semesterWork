fb256a6844e891ae9d4f8d6cb6ec8fa3a15fb613
CodyMaverick <bobkomatej1@gmail.com>
2023-11-07 08:37:54 +0100

    removed not needed variables from main

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,22 +1,13 @@
 package GUI;
 
-import javax.swing.*;
-
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Main {
     public static void main(String[] args) {
-        CardLayoutExample1 crdl = new CardLayoutExample1();
         Cards cards = new Cards();
-
-        crdl.setSize(500, 500);
-        crdl.setVisible(false);
-        crdl.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         cards.setSize(200, 200);
         cards.setVisible(true);
         cards.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        //CardLayoutDemo.createAndShowGUI();
     }
 }
